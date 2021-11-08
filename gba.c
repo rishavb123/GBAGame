31973177efcca0a5c89519d6dd8bcc7579960d10
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-11-07 19:22:33 -0500

    implemented some gba methods

--- gba.c
+++ gba.c
@@ -1,9 +1,10 @@
 #include "gba.h"
 
-volatile unsigned short *videoBuffer = (volatile unsigned short *) 0x6000000;
+volatile unsigned short *videoBuffer = (volatile unsigned short *)0x6000000;
 u32 vBlankCounter = 0;
 
-void waitForVBlank(void) {
+void waitForVBlank(void)
+{
     // (1)
     // Write a while loop that loops until we're NOT in vBlank anymore:
     // (This prevents counting one VBlank more than once if your app is too fast)
@@ -21,70 +22,80 @@ void waitForVBlank(void) {
 }
 
 static int __qran_seed = 42;
-static int qran(void) {
+static int qran(void)
+{
     __qran_seed = 1664525 * __qran_seed + 1013904223;
     return (__qran_seed >> 16) & 0x7FFF;
 }
 
 int randint(int min, int max) { return (qran() * (max - min) >> 15) + min; }
 
-void setPixel(int row, int col, u16 color) {
+void setPixel(int row, int col, u16 color)
+{
     videoBuffer[OFFSET(row, col, WIDTH)] = color;
 }
 
-void drawRectDMA(int row, int col, int width, int height, volatile u16 color) {
-    // TODO: IMPLEMENT
-    UNUSED(row);
-    UNUSED(col);
-    UNUSED(width);
-    UNUSED(height);
-    UNUSED(color);
-    for (int i = row; i < row + width; i++) {
+void drawRectDMA(int row, int col, int width, int height, volatile u16 color)
+{
+    for (int i = col; i < col + height; i++)
+    {
         DMA->src = &color;
-        DMA->dst = &videoBuffer[OFFSET(i, col, WIDTH)];
+        DMA->dst = &videoBuffer[OFFSET(row, i, WIDTH)];
         DMA->cnt = width | DMA_ON | DMA_SOURCE_FIXED | DMA_DESTINATION_DECREMENT;
     }
 }
 
-void drawFullScreenImageDMA(const u16 *image) {
-    // TODO: IMPLEMENT
-    UNUSED(image);
+void drawFullScreenImageDMA(const u16 *image)
+{
+    DMA->src = image;
+    DMA->dst = videoBuffer;
+    DMA->cnt = (WIDTH * HEIGHT) | DMA_ON | DMA_SOURCE_INCREMENT | DMA_DESTINATION_DECREMENT;
 }
 
-void drawImageDMA(int row, int col, int width, int height, const u16 *image) {
-    // TODO: IMPLEMENT
-    UNUSED(row);
-    UNUSED(col);
-    UNUSED(width);
-    UNUSED(height);
-    UNUSED(image);
+void drawImageDMA(int row, int col, int width, int height, const u16 *image)
+{
+    for (int i = col; i < col + height; i++)
+    {
+        DMA->src = image;
+        DMA->dst = &videoBuffer[OFFSET(row, i, WIDTH)];
+        DMA->cnt = width | DMA_ON | DMA_SOURCE_INCREMENT | DMA_DESTINATION_DECREMENT;
+    }
 }
 
-void fillScreenDMA(volatile u16 color) {
+void fillScreenDMA(volatile u16 color)
+{
     drawRectDMA(0, 0, WIDTH, HEIGHT, color);
 }
 
-void drawChar(int row, int col, char ch, u16 color) {
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (fontdata_6x8[OFFSET(j, i, 6) + ch * 48]) {
+void drawChar(int row, int col, char ch, u16 color)
+{
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (fontdata_6x8[OFFSET(j, i, 6) + ch * 48])
+            {
                 setPixel(row + j, col + i, color);
             }
         }
     }
 }
 
-void drawString(int row, int col, char *str, u16 color) {
-    while (*str) {
+void drawString(int row, int col, char *str, u16 color)
+{
+    while (*str)
+    {
         drawChar(row, col, *str++, color);
         col += 6;
     }
 }
 
-void drawCenteredString(int row, int col, int width, int height, char *str, u16 color) {
+void drawCenteredString(int row, int col, int width, int height, char *str, u16 color)
+{
     u32 len = 0;
     char *strCpy = str;
-    while (*strCpy) {
+    while (*strCpy)
+    {
         len++;
         strCpy++;
     }
