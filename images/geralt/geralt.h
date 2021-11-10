f6a974ab7651147858c905bb314e124c2e5bf0a6
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-11-10 15:47:37 -0500

    downloaded geralt image

---
+++ images/geralt/geralt.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=33x50 geralt geralt.png 
+ * Time-stamp: Wednesday 11/10/2021, 20:47:12
+ * 
+ * Image Information
+ * -----------------
+ * geralt.png 33@50
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef GERALT_H
+#define GERALT_H
+
+extern const unsigned short geralt[1650];
+#define GERALT_SIZE 3300
+#define GERALT_LENGTH 1650
+#define GERALT_WIDTH 33
+#define GERALT_HEIGHT 50
+
+#endif
+
