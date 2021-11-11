6c5145362d1d610a3a12261bfae469fad669a43e
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-11-10 21:32:31 -0500

    downloaded werewolf image

---
+++ images/monsters/werewolf/werewolf.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=53x50 werewolf werewolf.jpg 
+ * Time-stamp: Thursday 11/11/2021, 02:32:12
+ * 
+ * Image Information
+ * -----------------
+ * werewolf.jpg 53@50
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef WEREWOLF_H
+#define WEREWOLF_H
+
+extern const unsigned short werewolf[2650];
+#define WEREWOLF_SIZE 5300
+#define WEREWOLF_LENGTH 2650
+#define WEREWOLF_WIDTH 53
+#define WEREWOLF_HEIGHT 50
+
+#endif
+
